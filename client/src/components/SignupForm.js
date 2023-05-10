2d8a7eccdbe15b7b21254a130c5e1cf97f33f900
Gage Norton <nortongage0@gmail.com>
2023-05-10 12:59:54 -0500

    Fixed login bug

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -32,7 +32,7 @@ const SignupForm = () => {
         variables: { ...userFormData },
       });
 
-      Auth.login(data.addUser.token);
+      Auth.login(data.createUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
