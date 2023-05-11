0e72c1e9c3db77ca4be26b077bebe17043813bcc
Gage Norton <nortongage0@gmail.com>
2023-05-11 14:27:31 -0500

    Fix ternary bug

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -65,12 +65,12 @@ const SavedBooks = () => {
       </div>
       <Container>
         <h2 className='pt-5'>
-          {userData.savedBooks.length
+          {userData.savedBooks?.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <Row>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
               <Col md="4">
                 <Card key={book.bookId} border='dark'>
