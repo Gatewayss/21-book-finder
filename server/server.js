77f18eea9433bb30e2124c967c8615ae5269e7d1
Gage Norton <nortongage0@gmail.com>
2023-05-11 18:28:00 -0500

    test

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas'); 
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth')
 
